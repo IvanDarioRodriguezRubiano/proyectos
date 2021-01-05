175b77f9cc1d259676460430efc70d0778a9258b
IvanDarioRodriguezRubiano <ivdarodriguezrubiano@gmail.com>
2021-01-05 14:51:36 -0500

    agregando cambios para la cuenta de github

--- test/src/App.js
+++ test/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edith <code>src/App.js</code> and save to reload.
+          Edith para mis amigos <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
